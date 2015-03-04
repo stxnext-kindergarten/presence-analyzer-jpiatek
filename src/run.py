e50573cbed2c6e88217e779f07bdac8468d0bc2e
Jerzy Piatek <jerzy.piatek@stxnext.pl>
2015-03-04 15:16:13 +0100

    poprawka pylint dla run.py

--- src/run.py
+++ src/run.py
@@ -6,11 +6,10 @@ import os.path
 import logging.config
 
 from presence_analyzer.main import app
-import presence_analyzer.views
 
 
 if __name__ == "__main__":
-    ini_filename = os.path.join(os.path.dirname(__file__),
+    INI_FILENAME = os.path.join(os.path.dirname(__file__),
                                 '..', 'runtime', 'debug.ini')
-    logging.config.fileConfig(ini_filename, disable_existing_loggers=False)
+    logging.config.fileConfig(INI_FILENAME, disable_existing_loggers=False)
     app.run(host='0.0.0.0')
